55e8a5e21c62967826d64695fda46a351803aa80
MiguelRavello <errejota7@gmail.com>
2019-10-16 13:26:11 -0500

    Matriz de cambio de base

---
+++ matrices/cambioBase12.m
@@ -0,0 +1,12 @@
+% Matriz de cambio de base de 1 a 2
+% B1={u1,u2,...,un}
+% B2={v1,v2,...,vn}
+% u1=a1*v1+a2*v2+...+an*vn
+% (u1,u2,...,un)=(v1,v2,v3,...,vn)* Mb1b2
+% B1 = B2 * Mb1->b2
+% M=B2^-1 *B1
+function M=cambioBase12(A,B)
+  B1=A';
+  B2=B';
+  M= inversa(B2)*B1;
+endfunction
---
+++ matrices/esBase.m
@@ -0,0 +1,16 @@
+% Sea el conjunto de vectores
+% B={u1,u2,...,un}
+% Tal que B sea L.i.
+% Span(B)=V, V es un espacio vectorial
+function verdad=esBase(B)
+  [m n]=size(B);
+  ceros=zeros(1,m);
+  u=descomposicionPLU(B,ceros);
+  %u tiene q ser un vector cero, para q sea l.i.
+  for i=1:length(u)
+    if(u(i)!=0)
+      verdad=false;
+      break;
+    endif
+  endfor
+endfunction
